eafe04ace09f8dcc196196c21d377d709bfbb81e
flynnmcleod <50563245+flynnmcleod@users.noreply.github.com>
2019-07-16 23:37:55 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './Components/stylesheet.css'
+
+import Display from './Components/Display'
+
+
+class App extends Component {
+    render() {
+        return (<div>
+            <div>
+            <Display />
+            </div>
+
+        </div>
+        )
+    }
+
+}
+
+
+export default App;
